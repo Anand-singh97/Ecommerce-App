f330ceaeb4723335e0d37caf7aa9096abcdf0cbe
Anand Singh <pb11aps97@gmail.com>
2024-01-11 03:18:26 -0500

    routing paths added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,27 @@
-import './index.css';
-import Navbar from './Components/Navbar/Navbar';
-
+import "./index.css";
+import Navbar from "./Components/Navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
+import { Shop } from "./Pages/Shop";
+import { ShopCategory } from "./Pages/ShopCategory";
+import { Product } from "./Pages/Product";
+import { Cart } from "./Pages/Cart";
+import { LoginSignup } from "./Pages/LoginSignup";
 function App() {
   return (
     <div className="App font-poppins">
-      <Navbar/>
+      <Routes>
+        <Route path="/" element=<Navbar />>
+          <Route index element=<Shop /> />
+          <Route path="/men" element=<ShopCategory category="men" /> />
+          <Route path="/women" element=<ShopCategory category="women" /> />
+          <Route path="/kids" element=<ShopCategory category="kids" /> />
+          <Route path="/product" element=<Product />>
+            <Route path=":productId" element=<Product /> />
+          </Route>
+          <Route path="/cart" element=<Cart /> />
+          <Route path="/login" element=<LoginSignup /> />
+        </Route>
+      </Routes>
     </div>
   );
 }
