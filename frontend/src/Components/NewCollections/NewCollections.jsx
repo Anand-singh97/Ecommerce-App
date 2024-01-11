69d7648665a41b8d4fe16001255b64197ea6dc06
Anand Singh <pb11aps97@gmail.com>
2024-01-11 17:00:50 -0500

    NewsLetter component created.

--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -3,7 +3,7 @@ import newCollection from '../Assets/new_collections';
 import Item from '../Item/Item';
 const NewCollections = () => {
   return (
-    <div>
+    <div className='mb-[5rem]'>
         <div className='flex justify-center mb-4'>
             <h1 className='mb-1 text-xl font-bold border-b-[3px] border-black w-fit text-center mt-5'>NEW COLLECTIONS</h1>
         </div>
