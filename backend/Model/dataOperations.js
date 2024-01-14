70e6004966d424a61a1ae0d1db3907b773e02772
Anand Singh <pb11aps97@gmail.com>
2024-01-13 19:37:22 -0500

    methods created for add/delete and getAll product data.

---
+++ backend/Model/dataOperations.js
@@ -0,0 +1,63 @@
+const {productModel} = require('./product.model');
+
+
+async function addProduct(name, image, category, new_price, old_price)
+{
+    let ProductData = await productModel.find({});
+    let id = 1;
+    if(ProductData.length > 0)
+    {
+        let last_product_array = ProductData.slice(-1);
+        let lastProduct = last_product_array[0];
+        id = lastProduct.productId + 1;
+    }
+    try
+    {
+        await productModel.create({
+            productId:id,
+            name,
+            image,
+            category,
+            old_price,
+            new_price
+        });
+        return {success:true};
+    }
+    catch(error)
+    {
+        return ({success:false, message:error.message});
+    }
+    
+}
+
+async function removeProduct(id)
+{
+    try
+    {
+        await productModel.findOneAndDelete({productId:id});
+        return {success:true};
+    }
+    catch(error)
+    {
+        return ({success:false, message:error.message});
+    } 
+}
+
+async function getAllProducts()
+{
+    try
+    {
+        const products = await productModel.find({});
+        return({success:true, result:products});
+    }
+    catch(error)
+    {
+        return({success:false, message:error.message});
+    }
+}
+
+module.exports = {
+    addProduct,
+    removeProduct,
+    getAllProducts
+}
