bf91eddc491f7a669e16e581d2080a7c19bf7915
Anand Singh <pb11aps97@gmail.com>
2024-01-13 19:36:45 -0500

    mongoose model created for Product.

---
+++ backend/Model/product.model.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+
+    productId:{
+        type:Number,
+        required:true,
+    },
+    name:{
+        type:String,
+        required:true
+    },
+    image:{
+        type:String,
+        required:true
+    },
+    category:{
+        type:String,
+        required:true
+    },
+    new_price:{
+        type:Number,
+        required:true
+    },
+    old_price:{
+        type:Number,
+        requited:true
+    },
+    date:{
+        type: Date,
+        default:Date.now,
+    },
+    available:{
+        type:Boolean,
+        default:true
+    }
+});
+
+const productModel = mongoose.model('product', productSchema);
+
+module.exports = {
+    productModel
+};
