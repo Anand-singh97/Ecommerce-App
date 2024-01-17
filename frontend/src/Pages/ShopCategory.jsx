be1481e3c326a3e6062739e366a4725f51c70268
Anand Singh <pb11aps97@gmail.com>
2024-01-16 22:13:56 -0500

    sort by logic added

--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -1,15 +1,39 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../Components/ShopContext/ShopContext";
-import dropDownIcon from "../Components/Assets/dropdown_icon.png";
 import Item from "../Components/Item/Item";
-import {Circles} from 'react-loader-spinner';
+import { Circles } from "react-loader-spinner";
+import { FormControl, InputLabel, Select, MenuItem } from "@mui/material";
 
 export const ShopCategory = (props) => {
   const { allProducts } = useContext(ShopContext);
   const { banner, category } = props;
+  const [categorySpecificProducts, setCategorySpecificProducts] = useState([]);
+  const [sortBySelection, setSortBySelection] = useState('title');
+  useEffect(() => {
 
-  if(!allProducts)
-  {
+    if (allProducts.length > 0) {
+      const filteredProducts = allProducts.filter((product) => {
+        return product.category === category;
+      });
+
+      switch(sortBySelection)
+      {
+        case 'title':
+          filteredProducts.sort((a,b)=>a.name.localeCompare(b.name));
+          break;
+        case 'priceDescending':
+          filteredProducts.sort((a,b)=>b.new_price - a.new_price);
+          break;
+          case 'priceAscending':
+            filteredProducts.sort((a,b)=>a.new_price - b.new_price);
+            break;
+          default:
+      }
+      setCategorySpecificProducts(filteredProducts);
+    }
+  }, [allProducts, category, setCategorySpecificProducts, sortBySelection]);
+
+  if (!allProducts) {
     return (
       <div className="flex justify-center items-center h-screen">
         <Circles
@@ -25,27 +49,37 @@ export const ShopCategory = (props) => {
     <div className="flex flex-col gap-3">
       <img className="mx-3" src={banner} alt="banner" />
       <div className="flex flex-col gap-4">
-        <div className="flex justify-between">
+        <div className="flex items-center justify-between">
           <p className="ml-3">
-            <span className=" font-bold">Showing 1-12</span> out of 36 Products
+            <span className=" font-bold">
+              Showing 1-{categorySpecificProducts.length}
+            </span>{" "}
+            out of {allProducts.length} Products
           </p>
-          <div className="flex mr-3 border-2 rounded-full py-1 px-4 items-center gap-1">
-            <p>Sort by</p>
-            <div>
-              <img alt="drop down icon" src={dropDownIcon} />
-            </div>
+          <div className="flex w-[25%] md:w-[20%] lg:w-[10%] mr-[2rem] gap-1">
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Sort By</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                label="Sort By"
+                value={sortBySelection}
+                onChange={(e)=>setSortBySelection(e.target.value)}
+              >
+                <MenuItem value='title'>Title</MenuItem>
+                <MenuItem value='priceDescending'>Price(High to Low)</MenuItem>
+                <MenuItem value='priceAscending'>Price(Low to High)</MenuItem>
+              </Select>
+            </FormControl>
           </div>
         </div>
         <div className="grid grid-cols-1 lg:mx-3 gap-5 md:grid-cols-2 place-items-center lg:grid-cols-4">
-          {allProducts.map((item) => {
-            if (category === item.category) {
-              return (
-                <div key={item.productId}>
-                  <Item item={item} />
-                </div>
-              );
-            }
-            return null;
+          {categorySpecificProducts.map((item) => {
+            return (
+              <div key={item.productId}>
+                <Item item={item} />
+              </div>
+            );
           })}
         </div>
       </div>
