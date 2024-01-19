3369d1f9ad5b7e17737143553b3b6db87497bbff
Anand Singh <pb11aps97@gmail.com>
2024-01-19 02:33:50 -0500

    old navBar replaced with materialUI nav bar

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,92 +1,133 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import logo from "../Assets/logo.png";
 import { Link, Outlet } from "react-router-dom";
 import { ShopContext } from "../ShopContext/ShopContext";
-import { MdShoppingCart } from "react-icons/md";
-
+import profilePic from "../Assets/profilePic.webp";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 function Navbar() {
-  const [underline, setUnderline] = useState({
-    shop: "border-red-500 translate-y-[-0.2rem] border-b-[2.1px]",
-  });
   const { getTotalCartItems } = useContext(ShopContext);
-  const displayUnderline = (elementName) => {
-    setUnderline({
-      [elementName]: "border-red-500 translate-y-[-0.2rem] border-b-[2.1px]",
-    });
-  };
 
   const logout = () => {
     localStorage.removeItem("auth-token");
-    window.location.replace('/');
+    window.location.replace("/");
   };
 
   return (
-    <>
-      <div className="flex py-3 mx-2 items-center justify-between gap-[3rem] border-b-2">
-        <div className="flex md:ml-[2rem] justify-center items-center">
-          <Link to={"/"}>
-            <img
-              className=""
-              onClick={() =>
-                setUnderline({
-                  shop: "border-red-500 translate-y-[-0.2rem] border-b-[2.1px]",
-                })
-              }
-              src={logo}
-              alt="app logo"
-            />
-          </Link>
-        </div>
-
-        <div>
-          <ul className="flex text-[1rem] lg:gap-[2rem] items-center gap-3">
-            <li className={underline.shop ? underline.shop : ""}>
-              <Link onClick={() => displayUnderline("shop")} to={"/"}>
-                Shop
-              </Link>
-            </li>
-            <li className={underline.men ? underline.men : ""}>
-              <Link onClick={() => displayUnderline("men")} to={"/men"}>
-                Men
-              </Link>
-            </li>
-            <li className={underline.women ? underline.women : ""}>
-              <Link onClick={() => displayUnderline("women")} to={"/women"}>
-                Women
-              </Link>
-            </li>
-            <li className={underline.kids ? underline.kids : ""}>
-              <Link onClick={() => displayUnderline("kids")} to={"/kid"}>
-                Kids
-              </Link>
-            </li>
-          </ul>
-        </div>
-
-        <div className="flex justify-center md:mr-[2rem] gap-5 items-center mr-2">
-          <div className="relative">
-            <Link to={"/cart"}>
-              <MdShoppingCart className="scale-[2]" />
-            </Link>
-            <div className="absolute flex justify-center items-center top-[-1rem] text-white right-[-1rem] rounded-full h-[1.1rem] w-[1.1rem] bg-red-400">
-              <p className="text-[1rem] ">{getTotalCartItems()}</p>
+    <div>
+      <nav class="bg-gray-800 bg-transparent w-full">
+        <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div class="relative flex h-16 items-center justify-between">
+            <div class="flex items-center justify-center sm:items-stretch sm:justify-start">
+              <div class="justify-start">
+                <Link to={"/"} >
+                  <img className="w-[70%]" src={logo} alt="shopping logo" />
+                </Link>
+              </div>
+              <div className="hidden sm:ml-6 sm:block">
+                <div className="flex space-x-4">
+                  <Link
+                    to={"/"}
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                    aria-current="page"
+                  >
+                    Shop
+                  </Link>
+                  <Link
+                    to={"/men"}
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                    aria-current="page"
+                  >
+                    Men
+                  </Link>
+                  <Link
+                    to={"/women"}
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                  >
+                    Women
+                  </Link>
+                  <Link
+                    to={"/kid"}
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                  >
+                    Kid
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              <div class="flex ml-3 items-center gap-5 mr-2">
+                {localStorage.getItem("auth-token") ? (
+                  <Link
+                    onClick={logout}
+                    className="text-gray-300 border-[1px] hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                  >
+                    Logout
+                  </Link>
+                ) : (
+                  <Link
+                    to={"/login"}
+                    className="text-gray-300 border-[1px] hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                  >
+                    Login
+                  </Link>
+                )}
+                <div>
+                  <Link
+                    to={
+                      localStorage.getItem("auth-token") ? "/profile" : "/login"
+                    }
+                    className=""
+                    id="user-menu-button"
+                    aria-expanded="false"
+                    aria-haspopup="true"
+                  >
+                    <img class="h-8 w-8 rounded-full" src={profilePic} alt="" />
+                  </Link>
+                </div>
+                <div className="relative">
+                  <Link to={"/cart"}>
+                    <ShoppingCartIcon className="scale-[1.4] text-white" />
+                  </Link>
+                  <div className="absolute flex justify-center items-center top-[-0.5rem] text-white right-[-0.6rem] rounded-full h-[1.1rem] w-[1.1rem] bg-red-400">
+                    <p className="text-[0.8rem]">{getTotalCartItems()}</p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-          {localStorage.getItem("auth-token") ? (
-            <Link onClick={logout} className="border-2 bg-orange-300 py-1 px-5 rounded-lg">
-              Logout
-            </Link>
-          ) : (
-            <Link className="border-2 py-1 bg-orange-300 px-5 rounded-lg" to={"/login"}>
-              Login
-            </Link>
-          )}
         </div>
-      </div>
-      <div>
-        <Outlet />
-      </div>
-    </>
+
+        <div class="sm:hidden" id="mobile-menu">
+          <div class="flex justify-center items-center gap-3 px-2 pb-3 pt-2">
+            <Link
+              to={"/"}
+              class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+            >
+              Shop
+            </Link>
+            <Link
+              to={"/men"}
+              class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+            >
+              Men
+            </Link>
+            <Link
+              to={"/women"}
+              class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+            >
+              Women
+            </Link>
+            <Link
+              to={"/kid"}
+              class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+            >
+              Kid
+            </Link>
+          </div>
+        </div>
+      </nav>
+      <Outlet />
+    </div>
   );
 }
 
